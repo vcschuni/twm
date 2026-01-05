290f5706b0082a95e4b8149c83fd9285c1cbb7b7
Schunicht <volker.schunicht@gov.bc.ca>
2026-01-05 11:57:00 -0800

    Delete twm src

--- compose/apache-php/src/application/.svn/pristine/ff/ff74fd1ee64e1f933677c8c7a7cf646616004c58.svn-base
+++
@@ -1,4 +0,0 @@
-import 'ol/ol.css';
-import olms from 'ol-mapbox-style';
-
-olms('map', 'data/wms.json');
--- compose/apache-php/src/application/lib/ol-mapbox-style-12.2.0/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as MapboxVectorLayer } from "./MapboxVectorLayer.js";
-export { stylefunction, recordStyleLayer, renderTransparent, getStyleForLayer } from "./stylefunction.js";
-export { apply as default, apply, applyBackground, applyStyle, getLayer, getLayers, getSource, getMapboxLayer, updateMapboxSource, updateMapboxLayer, addMapboxLayer, removeMapboxLayer, getFeatureState, setFeatureState } from "./apply.js";
